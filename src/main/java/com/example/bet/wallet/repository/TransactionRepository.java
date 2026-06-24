97813a4ce90eecfcee0e7d53594118f7d3917769
renato <guerrerorenatojr@gmail.com>
2026-06-25 05:10:22 +0800

    add user history

--- src/main/java/com/example/bet/wallet/repository/TransactionRepository.java
+++ src/main/java/com/example/bet/wallet/repository/TransactionRepository.java
@@ -3,5 +3,9 @@ package com.example.bet.wallet.repository;
 import com.example.bet.wallet.entity.Transaction;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends CrudRepository<Transaction, Long>{
+
+    List<Transaction> findByUserId(Long userId);
 }
