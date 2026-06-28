8baaf17acecb3a0c4de2c5ada7d96442dfc8a9a8
renato <guerrerorenatojr@gmail.com>
2026-06-28 09:08:19 +0800

    add React UI skeleton

--- src/main/java/com/example/bet/user/dto/CreateUserRequest.java
+++ src/main/java/com/example/bet/user/dto/CreateUserRequest.java
@@ -1,6 +1,12 @@
 package com.example.bet.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CreateUserRequest(
-    String username
+
+        @NotBlank
+        @Size(min = 3, max = 100)
+        String username
 ) {
 }
